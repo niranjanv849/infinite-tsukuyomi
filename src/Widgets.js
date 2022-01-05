aa903d08f119b724167142a23ad12e9baac268b1
Niranjan849 <niranjan.ct19@bitsathy.ac.in>
2022-01-05 12:10:46 +0530

    adjusted favicon and public sources

--- src/Widgets.js
+++ src/Widgets.js
@@ -5,17 +5,17 @@ import "./Widgets.css";
 //   TwitterShareButton,
 //   TwitterTweetEmbed,
 // } from "react-twitter-embed";
-// import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
       <div className="widgets__input">
-        {/* <SearchIcon className="widgets__searchIcon" />
-        <input placeholder="Search Twitter" type="text" />
+        <SearchIcon className="widgets__searchIcon" />{"  "}
+        <input placeholder=" Search by @username" type="text" />
       </div>
 
-      <div className="widgets__widgetContainer">
+      {/* <div className="widgets__widgetContainer">
         <h2>What's happening</h2>
 
         <TwitterTweetEmbed tweetId={"858551177860055040"} />
@@ -29,8 +29,8 @@ function Widgets() {
         <TwitterShareButton
           url={"https://facebook.com/cleverprogrammer"}
           options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
-        /> */}
-      </div>
+        />
+      </div> */}
     </div>
   );
 }

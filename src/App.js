04978c21806e59090b50c3afea1049f65239902d
Niranjan849 <niranjan.ct19@bitsathy.ac.in>
2022-01-05 10:23:07 +0530

    Lat

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     // BEM
     <div className="app">
       <Sidebar />
-      {/* <Feed />
-      <Widgets /> */}
+      <Feed />
+      <Widgets />
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { Button } from "@material-ui/core";
-import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
-    
+// import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      {/* <img className="logo" src={logo} alt="LOGO" /> */}
-      <LocalFireDepartmentIcon className="sidebar__MainIcon" />
+      <TwitterIcon className="sidebar__twitterIcon" />
 
       <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={PermIdentityIcon} text="Profile" />
